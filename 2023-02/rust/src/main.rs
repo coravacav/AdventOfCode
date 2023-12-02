b28b02d1f6649964d4690b712438f9e0b492adb3
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-12-01 21:55:56 -0700

    add day 2

---
+++ 2023-02/rust/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    println!("part1: {}", part1(input));
+    println!("part2: {}", part2(input));
+}
+
+fn part1(input: &str) -> usize {
+    0
+}
+
+fn part2(input: &str) -> usize {
+    0
+}
