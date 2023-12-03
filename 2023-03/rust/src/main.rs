0138d5179a7b755cb78807309428c0da5894e1c7
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-12-03 05:42:50 -0700

    unoptimized

--- 2023-03/rust/src/main.rs
+++ 2023-03/rust/src/main.rs
@@ -151,93 +151,249 @@ fn part2(input: &str) -> usize {
         .sum()
 }
 
+#[cfg(not(debug_assertions))]
 const LINE_WIDTH: usize = {
     let mut line_width = 0;
     while include_str!("../input.txt").as_bytes()[line_width] != b'\n' {
         line_width += 1;
     }
-    line_width
+    line_width + 1
 };
 
-const LINES: usize = include_str!("../input.txt").as_bytes().len() / LINE_WIDTH;
+// when in release mode, we can use the const
+#[cfg(not(debug_assertions))]
+const INPUT_SIZE: usize = include_str!("../input.txt").len();
+
+#[cfg(debug_assertions)]
+const LINE_WIDTH: usize = 11;
+
+#[cfg(debug_assertions)]
+const INPUT_SIZE: usize = 110;
 
 fn speedy_part_2(input: &str) -> usize {
-    let mut mat = [[0u16; LINE_WIDTH]; LINES];
-    let mut line = 0;
-    let mut col = 0;
-
-    let mut star_locations = Vec::new();
-    let mut number_values = Vec::new();
-    number_values.push(0);
-    number_values.push(0);
-    let mut val = 0;
-
-    for c in input.as_bytes() {
-        match c {
-            b'*' => {
-                mat[line][col] = 1;
-                star_locations.push((line, col));
-                col += 1;
-            }
-            c @ b'0'..=b'9' => {
-                incr_num!(val, c);
-                mat[line][col] = number_values.len() as u16;
-                col += 1;
-                continue;
-            }
-            b'\n' => {
-                line += 1;
-                col = 0;
-            }
-            _ => {
-                col += 1;
-            }
-        };
-
-        if val > 0 {
-            number_values.push(val);
-            val = 0;
-        }
-    }
-
-    star_locations
-        .into_iter()
-        .filter_map(|(x, y)| {
-            let mut seen = [0; 2];
-
-            for (i, j) in [
-                (x, y - 1),
-                (x, y + 1),
-                (x - 1, y - 1),
-                (x - 1, y),
-                (x - 1, y + 1),
-                (x + 1, y - 1),
-                (x + 1, y),
-                (x + 1, y + 1),
-            ] {
-                if mat[i][j] > 1 {
-                    match seen {
-                        [a, _] | [_, a] if a == mat[i][j] => {}
-                        [0, _] => {
-                            seen[0] = mat[i][j];
-                        }
-                        [_, 0] => {
-                            seen[1] = mat[i][j];
-                            break; // Technically should keep checking but all inputs I've found allow this.
-                        }
-                        _ => return None,
-                    }
-                }
+    input
+        .as_bytes()
+        .iter()
+        .enumerate()
+        .map(|(i, c)| {
+            if i < LINE_WIDTH + 1
+                || i >= INPUT_SIZE - LINE_WIDTH - 1
+                || i % LINE_WIDTH == 0
+                || i % LINE_WIDTH == LINE_WIDTH - 1
+            {
+                return 0;
             }
 
-            Some(number_values[seen[0] as usize] * number_values[seen[1] as usize])
+            match c {
+                b'*' => handle_star(input.as_bytes(), i),
+                _ => 0,
+            }
         })
         .sum()
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Search {
+    None,
+    TopLeftUnbound,
+    TopLeftPartial,
+    TopMiddle,
+    TopFull,
+    TopRightPartial,
+    TopRightUnbound,
+    RightUnbound,
+    BottomRightUnbound,
+    BottomRightPartial,
+    BottomMiddle,
+    BottomFull,
+    BottomLeftPartial,
+    BottomLeftUnbound,
+    LeftUnbound,
+}
+
+fn handle_star(input: &[u8], i: usize) -> usize {
+    let mut first = Search::None;
+    let mut second = Search::None;
+
+    macro_rules! assign_or_return_0 {
+        ($first:ident, $second:ident, $val:expr) => {
+            if matches!($first, Search::None) {
+                $first = $val;
+            } else if matches!($second, Search::None) {
+                $second = $val;
+            } else {
+                return 0;
+            }
+        };
+    }
+
+    /// walk_left
+    fn w_l(input: &[u8], start: usize) -> usize {
+        let mut i = start;
+        while matches!(input[i], b'0'..=b'9') {
+            if i == 0 {
+                return 0;
+            }
+            i -= 1;
+        }
+        i + 1
+    }
+
+    /// walk_right
+    fn w_r(input: &[u8], start: usize) -> usize {
+        let mut i = start;
+        while matches!(input[i], b'0'..=b'9') {
+            if i == INPUT_SIZE - 2 {
+                break;
+            }
+            i += 1;
+        }
+        i
+    }
+
+    fn read_precise(input: &[u8], start: usize, end: usize) -> usize {
+        let mut number_iter = &input[start..=end];
+        let mut val = 0;
+
+        // Walk enough left to be sure we're at the start of the number
+
+        while let [c @ b'0'..=b'9', ..] = number_iter {
+            val = val * 10 + (c - b'0') as usize;
+            number_iter = &number_iter[1..];
+        }
+
+        val
+    }
+
+    fn parse(input: &[u8], i: usize, search_location: Search) -> usize {
+        match search_location {
+            Search::None => 0,
+            Search::TopLeftUnbound => {
+                read_precise(input, w_l(input, i - LINE_WIDTH - 1), i - LINE_WIDTH + 1)
+            }
+            Search::TopLeftPartial => {
+                read_precise(input, w_l(input, i - LINE_WIDTH - 1), i - LINE_WIDTH)
+            }
+            Search::TopMiddle => read_precise(input, i - LINE_WIDTH, i - LINE_WIDTH),
+            Search::TopFull => read_precise(
+                input,
+                w_l(input, i - LINE_WIDTH - 1),
+                w_r(input, i - LINE_WIDTH + 1),
+            ),
+            Search::TopRightPartial => {
+                read_precise(input, i - LINE_WIDTH, w_r(input, i - LINE_WIDTH + 1))
+            }
+            Search::TopRightUnbound => {
+                read_precise(input, i - LINE_WIDTH + 1, w_r(input, i - LINE_WIDTH + 1))
+            }
+            Search::RightUnbound => read_precise(input, i + 1, w_r(input, i + 1)),
+            Search::BottomRightUnbound => {
+                read_precise(input, i + LINE_WIDTH + 1, w_r(input, i + LINE_WIDTH + 1))
+            }
+            Search::BottomRightPartial => {
+                read_precise(input, i + LINE_WIDTH, w_r(input, i + LINE_WIDTH + 1))
+            }
+            Search::BottomMiddle => read_precise(input, i + LINE_WIDTH, i + LINE_WIDTH),
+            Search::BottomFull => read_precise(
+                input,
+                w_l(input, i + LINE_WIDTH - 1),
+                w_r(input, i + LINE_WIDTH + 1),
+            ),
+            Search::BottomLeftPartial => {
+                read_precise(input, w_l(input, i + LINE_WIDTH - 1), i + LINE_WIDTH)
+            }
+            Search::BottomLeftUnbound => {
+                read_precise(input, w_l(input, i + LINE_WIDTH - 1), i + LINE_WIDTH - 1)
+            }
+            Search::LeftUnbound => read_precise(input, w_l(input, i - 1), i - 1),
+        }
+    }
+
+    match [
+        input[i - LINE_WIDTH - 1],
+        input[i - LINE_WIDTH],
+        input[i - LINE_WIDTH + 1],
+    ] {
+        [b'0'..=b'9', b'0'..=b'9', b'0'..=b'9'] => {
+            assign_or_return_0!(first, second, Search::TopFull);
+        }
+        [b'0'..=b'9', _, b'0'..=b'9'] => {
+            assign_or_return_0!(first, second, Search::TopLeftUnbound);
+            assign_or_return_0!(first, second, Search::TopRightUnbound);
+        }
+        [b'0'..=b'9', b'0'..=b'9', _] => {
+            assign_or_return_0!(first, second, Search::TopLeftPartial);
+        }
+        [_, b'0'..=b'9', b'0'..=b'9'] => {
+            assign_or_return_0!(first, second, Search::TopRightPartial);
+        }
+        [b'0'..=b'9', _, _] => {
+            assign_or_return_0!(first, second, Search::TopLeftUnbound);
+        }
+        [_, b'0'..=b'9', _] => {
+            assign_or_return_0!(first, second, Search::TopMiddle);
+        }
+        [_, _, b'0'..=b'9'] => {
+            assign_or_return_0!(first, second, Search::TopRightUnbound);
+        }
+        _ => {}
+    }
+
+    match input[i - 1] {
+        b'0'..=b'9' => {
+            assign_or_return_0!(first, second, Search::LeftUnbound);
+        }
+        _ => {}
+    }
+
+    match input[i + 1] {
+        b'0'..=b'9' => {
+            assign_or_return_0!(first, second, Search::RightUnbound);
+        }
+        _ => {}
+    }
+
+    match [
+        input[i + LINE_WIDTH - 1],
+        input[i + LINE_WIDTH],
+        input[i + LINE_WIDTH + 1],
+    ] {
+        [b'0'..=b'9', b'0'..=b'9', b'0'..=b'9'] => {
+            assign_or_return_0!(first, second, Search::BottomFull);
+        }
+        [b'0'..=b'9', _, b'0'..=b'9'] => {
+            assign_or_return_0!(first, second, Search::BottomLeftUnbound);
+            assign_or_return_0!(first, second, Search::BottomRightUnbound);
+        }
+        [b'0'..=b'9', b'0'..=b'9', _] => {
+            assign_or_return_0!(first, second, Search::BottomLeftPartial);
+        }
+        [_, b'0'..=b'9', b'0'..=b'9'] => {
+            assign_or_return_0!(first, second, Search::BottomRightPartial);
+        }
+        [b'0'..=b'9', _, _] => {
+            assign_or_return_0!(first, second, Search::BottomLeftUnbound);
+        }
+        [_, b'0'..=b'9', _] => {
+            assign_or_return_0!(first, second, Search::BottomMiddle);
+        }
+        [_, _, b'0'..=b'9'] => {
+            assign_or_return_0!(first, second, Search::BottomRightUnbound);
+        }
+        _ => {}
+    }
+
+    if first == Search::None || second == Search::None {
+        return 0;
+    }
+
+    parse(input, i, first) * parse(input, i, second)
+}
+
 #[test]
 fn test_part1() {
-    let test = r#"467..114..
+    let test = r#"
+467..114..
 ...*......
 ..35..633.
 ......#...
@@ -252,7 +408,8 @@ fn test_part1() {
 
 #[test]
 fn test_part2() {
-    let test = r#"467..114..
+    let test = r#"
+467..114..
 ...*......
 ..35..633.
 ......#...
@@ -275,7 +432,7 @@ fn test_speedy_part_2() {
 .....+.58.
 ..592.....
 ......755.
-...$.*....
-.664.598.."#;
+...$...*..
+.664...598"#;
     assert_eq!(speedy_part_2(test), 467835);
 }
