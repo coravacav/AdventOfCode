341bb363f87ea33a1cbad7008b2c3abeab72da47
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-12-02 22:35:32 -0700

    part 1

--- 2023-03/rust/src/main.rs
+++ 2023-03/rust/src/main.rs
@@ -1,5 +1,9 @@
 #![allow(unused)]
+use std::{cell::Cell, rc::Rc};
+
+use itertools::Itertools;
 use rust_aoc_lib::*;
+use tap::Tap;
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -18,23 +22,126 @@ fn main() {
     // simple_benchmark!(speedy_part_2, input);
 }
 
-const MAX_RED: usize = 12;
-const MAX_GREEN: usize = 13;
-const MAX_BLUE: usize = 14;
-
 fn part1(input: &str) -> usize {
-    0
+    let mut all_nums_and_locations = Vec::new();
+    for (linei, line) in input.lines().enumerate() {
+        let mut val = Rc::new(Cell::new(0));
+        let mut max = line.len();
+        for (i, c) in line.chars().enumerate() {
+            if c == '.' {
+                val = Rc::new(Cell::new(0));
+                continue;
+            }
+            if c.is_numeric() {
+                all_nums_and_locations.push((linei, i, Rc::clone(&val)));
+
+                val.replace(val.get() * 10 + (c as usize - '0' as usize));
+                continue;
+            }
+            val = Rc::new(Cell::new(0));
+        }
+    }
+
+    let mut all_valid_locations = Vec::new();
+
+    for (linei, line) in input.lines().enumerate() {
+        let mut max = line.len();
+        for (i, c) in line.chars().enumerate() {
+            if c == '.' || c.is_numeric() {
+                continue;
+            }
+
+            all_valid_locations.push((linei + 1, i));
+            all_valid_locations.push((linei, i));
+            all_valid_locations.push((linei.saturating_sub(1), i));
+            all_valid_locations.push((linei + 1, i + 1));
+            all_valid_locations.push((linei, i + 1));
+            all_valid_locations.push((linei.saturating_sub(1), i + 1));
+            all_valid_locations.push((linei + 1, i.saturating_sub(1)));
+            all_valid_locations.push((linei, i.saturating_sub(1)));
+            all_valid_locations.push((linei.saturating_sub(1), i.saturating_sub(1)));
+        }
+    }
+
+    all_nums_and_locations
+        .into_iter()
+        .filter(|(linei, i, val)| {
+            all_valid_locations
+                .iter()
+                .any(|(linei2, i2)| linei == linei2 && i == i2)
+        })
+        .unique_by(|(linei, i, val)| Rc::as_ptr(val) as usize)
+        .map(|(linei, i, val)| val.get())
+        .sum()
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let mut all_nums_and_locations = Vec::new();
+    for (linei, line) in input.lines().enumerate() {
+        let mut val = Rc::new(Cell::new(0));
+        let mut max = line.len();
+        for (i, c) in line.chars().enumerate() {
+            if c == '.' {
+                val = Rc::new(Cell::new(0));
+                continue;
+            }
+            if c.is_numeric() {
+                all_nums_and_locations.push((linei, i, Rc::clone(&val)));
+
+                val.replace(val.get() * 10 + (c as usize - '0' as usize));
+                continue;
+            }
+            val = Rc::new(Cell::new(0));
+        }
+    }
+
+    let mut all_valid_locations = Vec::new();
+
+    for (linei, line) in input.lines().enumerate() {
+        let mut max = line.len();
+        for (i, c) in line.chars().enumerate() {
+            if c == '.' || c.is_numeric() {
+                continue;
+            }
+
+            all_valid_locations.push((linei + 1, i));
+            all_valid_locations.push((linei, i));
+            all_valid_locations.push((linei.saturating_sub(1), i));
+            all_valid_locations.push((linei + 1, i + 1));
+            all_valid_locations.push((linei, i + 1));
+            all_valid_locations.push((linei.saturating_sub(1), i + 1));
+            all_valid_locations.push((linei + 1, i.saturating_sub(1)));
+            all_valid_locations.push((linei, i.saturating_sub(1)));
+            all_valid_locations.push((linei.saturating_sub(1), i.saturating_sub(1)));
+        }
+    }
+
+    all_nums_and_locations
+        .into_iter()
+        .filter(|(linei, i, val)| {
+            all_valid_locations
+                .iter()
+                .any(|(linei2, i2)| linei == linei2 && i == i2)
+        })
+        .unique_by(|(linei, i, val)| Rc::as_ptr(val) as usize)
+        .map(|(linei, i, val)| val.get())
+        .sum()
 }
 
-// #[test]
-// fn test_part1() {
-//     let test = r#""#;
-//     assert_eq!(part1(test), ___);
-// }
+#[test]
+fn test_part1() {
+    let test = r#"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."#;
+    assert_eq!(part1(test), 4361);
+}
 
 // #[test]
 // fn test_part2() {
