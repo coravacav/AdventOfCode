edc10bbfb4ac9baacd67aa730fa5d11e45199d33
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-12-03 23:28:05 -0700

    speedy part 1 take 1

--- 2023-04/rust/src/main.rs
+++ 2023-04/rust/src/main.rs
@@ -9,15 +9,15 @@ fn main() {
 
     println!("part1: {}", part1(input));
     println!("part2: {}", part2(input));
-    // println!("speedy_part_1: {}", speedy_part_1(input));
+    println!("speedy_part_1: {}", speedy_part_1(input));
     // println!("speedy_part_2: {}", speedy_part_2(input));
 
-    // assert_eq_same_input!(input, part1, speedy_part_1);
+    assert_eq_same_input!(input, part1, speedy_part_1);
     // assert_eq_same_input!(input, part2, speedy_part_2);
 
-    // simple_benchmark!(part1, input);
-    // simple_benchmark!(speedy_part_1, input);
-    // simple_benchmark!(part2, input);
+    simple_benchmark!(part1, input, 10000);
+    simple_benchmark!(speedy_part_1, input, 10000);
+    simple_benchmark!(part2, input, 10000);
     // simple_benchmark!(speedy_part_2, input);
 }
 
@@ -103,6 +103,66 @@ fn part2(input: &str) -> usize {
         .1
 }
 
+fn speedy_part_1(input: &str) -> usize {
+    let mut ans = [0u8; 10];
+    let mut nums = [0u8; 25];
+
+    let mut i = 0;
+    let mut target: &mut [u8] = &mut ans;
+    let mut val = 0;
+    let mut total = 0;
+
+    for c in input.as_bytes() {
+        match c {
+            b'|' => {
+                target = &mut nums;
+                i = 0;
+            }
+            b'\n' => {
+                target[i] = val as u8;
+
+                let mut points = 0;
+
+                for val in nums.iter().take_while(|val| **val > 0) {
+                    if ans.contains(val) {
+                        if points == 0 {
+                            points = 1;
+                        } else {
+                            points *= 2;
+                        }
+                    }
+                }
+
+                total += points;
+
+                i = 0;
+                val = 0;
+                ans = [0u8; 10];
+                nums = [0u8; 25];
+                target = &mut ans;
+            }
+            b'0'..=b'9' => {
+                val *= 10;
+                val += (c - b'0') as usize;
+            }
+            b' ' => {
+                if val > 0 {
+                    target[i] = val as u8;
+                    i += 1;
+                }
+
+                val = 0;
+            }
+            b':' => {
+                val = 0;
+            }
+            _ => {}
+        }
+    }
+
+    total
+}
+
 #[test]
 fn test_part1() {
     let test = r#"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
@@ -123,4 +183,15 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#;
     assert_eq!(part2(test), 30);
+}
+
+#[test]
+fn test_speedy_part_1() {
+    let test = r#"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#;
+    assert_eq!(part1(test), speedy_part_1(test));
 }
