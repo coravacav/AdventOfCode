b7bb34b8e36fd02da69f51c60c1a41f202c0eb11
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-12-06 20:11:09 -0700

    use functioning versions of everything

--- 2023-04/rust/src/part2.rs
+++ 2023-04/rust/src/part2.rs
@@ -11,14 +11,12 @@ fn part2(input: &str) -> usize {
         .map(|line| line.split_once(" | ").unwrap())
         .map(|(winners, ours)| {
             cards.clear();
-            winners
-                .split_ascii_whitespace()
-                .map(|win| win.parse::<usize>().unwrap())
-                .for_each(|card| {
-                    cards.push(card);
-                });
+            for win in winners.split_whitespace() {
+                let win = win.parse::<usize>().unwrap();
+                cards.push(win);
+            }
 
-            ours.split_ascii_whitespace()
+            ours.split_whitespace()
                 .map(|x| x.parse::<usize>().unwrap())
                 .filter_map(|x| if cards.contains(&x) { Some(x) } else { None })
                 .count()
@@ -29,9 +27,10 @@ fn part2(input: &str) -> usize {
                 let copies = copy_tracker.pop_front().unwrap_or(1);
 
                 for i in 0..x {
-                    match copy_tracker.get_mut(i) {
-                        Some(val) => *val += copies,
-                        None => copy_tracker.push_back(copies),
+                    if let Some(val) = copy_tracker.get_mut(i) {
+                        *val += copies;
+                    } else {
+                        copy_tracker.push_back(copies + 1);
                     }
                 }
 
