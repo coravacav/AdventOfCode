e7f3e2d80c5d467b9cba051e8fbe7cbcb7585676
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-12-07 22:07:01 -0700

    part 1

--- 2023-08/rust/src/part1.rs
+++ 2023-08/rust/src/part1.rs
@@ -1,7 +1,51 @@
+use std::collections::HashMap;
+
 use rust_aoc_lib::part1;
 
+enum Instruction {
+    Left,
+    Right,
+}
+
 #[part1]
-#[allow(unused_variables)]
 pub fn part1(input: &str) -> usize {
-    std::usize::MAX
+    let mut map: HashMap<&str, (&str, &str)> = HashMap::new();
+    let mut steps = Vec::new();
+    let mut lines = input.lines();
+
+    for char in lines.next().unwrap().chars() {
+        match char {
+            'L' => steps.push(Instruction::Left),
+            'R' => steps.push(Instruction::Right),
+            _ => panic!("Invalid input"),
+        }
+    }
+
+    let mut allowed_steps = steps.iter().cycle();
+
+    lines.next();
+
+    for line in lines {
+        let (start, rest) = line.split_once(" = (").unwrap();
+        let (left, rest) = rest.split_once(", ").unwrap();
+        let (right, _) = rest.split_once(')').unwrap();
+
+        map.insert(start, (left, right));
+    }
+
+    let mut current = "AAA";
+    let mut steps = 0;
+
+    while !matches!(current, "ZZZ") {
+        let (left, right) = map.get(current).unwrap();
+
+        steps += 1;
+
+        match allowed_steps.next().unwrap() {
+            Instruction::Left => current = left,
+            Instruction::Right => current = right,
+        }
+    }
+
+    steps
 }
--- 2023-08/rust/src/part2.rs
+++ 2023-08/rust/src/part2.rs
@@ -1,7 +1,6 @@
 use rust_aoc_lib::part2;
 
 #[part2]
-#[allow(unused_variables)]
 pub fn part2(input: &str) -> usize {
     std::usize::MAX
 }
