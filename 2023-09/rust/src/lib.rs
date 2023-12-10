85a378f0430956ef6da18ced689ee3a02d159716
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-12-09 17:06:17 -0700

    speedy part 1

--- 2023-09/rust/src/lib.rs
+++ 2023-09/rust/src/lib.rs
@@ -1,5 +1,5 @@
 pub mod part1;
 pub mod part2;
-pub mod speedy_part2;
+pub mod speedy;
 
 rust_aoc_lib::setup_distributed!();
--- 2023-09/rust/src/speedy.rs
+++ 2023-09/rust/src/speedy.rs
@@ -2,6 +2,32 @@ use itertools::Itertools;
 use rust_aoc_lib::part2;
 
 #[part2]
+pub fn speedy_part1(input: &str) -> isize {
+    input
+        .lines()
+        .map(|line| {
+            std::iter::successors(
+                Some(
+                    line.split_ascii_whitespace()
+                        .map(|x| x.parse::<isize>().unwrap())
+                        .collect_vec(),
+                ),
+                |prev| {
+                    (!prev.iter().all(|&x| x == 0))
+                        .then(|| prev.windows(2).map(|arr| arr[1] - arr[0]).collect_vec())
+                },
+            )
+            .collect_vec()
+            .iter()
+            .rev()
+            .skip(1)
+            .map(|x| x.last().unwrap())
+            .sum::<isize>()
+        })
+        .sum()
+}
+
+#[part2]
 pub fn speedy_part2(input: &str) -> isize {
     input
         .lines()
